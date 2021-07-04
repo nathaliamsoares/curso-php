276fd3c600ffbfc9b5a51daf4b3f9d5838458f0e
Guilherme Filippini <nathaliamsoares0@gmail.com>
2021-07-04 18:49:49 -0300

    aula de variaveis dinamicas

---
+++ 03-variaveis-dinamicas.php
@@ -0,0 +1,15 @@
+<?php
+// Variáveis dinâmicas
+$bebida = "refrigerante";
+
+$$bebida = "Guaraná";
+
+echo $refrigerante;
+echo "<br>";
+echo "<hr>";
+/////////////////////////////
+
+$destino = "cidade";
+$$destino = "Porto Alegre";
+
+echo $cidade;
