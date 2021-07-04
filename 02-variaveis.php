9f2076419356acfa13af6680ac164dc9469e7847
Guilherme Filippini <nathaliamsoares0@gmail.com>
2021-07-04 18:46:45 -0300

    aula de variaveis

---
+++ 02-variaveis.php
@@ -0,0 +1,31 @@
+<?php
+// Meus dados
+$nome = "Nathalia Machado";
+$idade = 21;
+$altura = 1.60; 
+
+echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura <br>";
+
+// Alterando meus dados
+$nome = "Guilherme Filippini";
+$idade = 20;
+$altura = 1.68;
+
+echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura <br>";
+echo "<hr>";
+
+$carro = "Hilux";
+echo "$carro";
+
+// Nomes válidos de variáveis
+//$nome
+//$_meunome
+//$carro10
+//$nota1
+//$meu_nome
+//$meuNome
+
+// Nomes inválidos
+//$123456nome
+//$carro!@$#
+//$meu carro
