62fa55aaec4b16b5e162b33767283fe0837dfbd4
Guilherme Filippini <nathaliamsoares0@gmail.com>
2021-07-04 18:53:25 -0300

    aula de aspas e concatenacao

---
+++ 05-aspas-concatenacao.php
@@ -0,0 +1,7 @@
+<?php
+$nome = "Nathalia Machado";
+echo 'Meu nome é '.$nome. ' e minha idade é \'21\'';
+// as aspas simples são literais, mesmo que haja variáveis, elas não são interpretadas./////
+// tudo o que estiver dentro de aspas simples será considerado como texto.///////
+echo "<hr>"; 
+echo "Meu nome é $nome";
